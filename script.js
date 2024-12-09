cd6372fa05bbf678b8f804e30031fcd7ecdd84d8
Siddhant1423 <130544314+Siddhant1423@users.noreply.github.com>
2024-12-10 00:26:45 +0530

    Add CSS file and made changes to JS file.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-div = document.querySelector("#container");
+const headingDiv = document.createElement("div");
+headingDiv.id = "heading";
+const heading = document.createElement("h1");
+heading.textContent = "Etch-a-Sketch";
+const btn = document.createElement("button");
+btn.textContent = "Change grid size"
+btn.id = "btn-resize";
+document.body.prepend(headingDiv);
+headingDiv.appendChild(heading)
+headingDiv.appendChild(btn)
+
+const div = document.querySelector("#container");
+
+for(let i = 0 ; i < 16*16 ; i++){
+    const divBlock = document.createElement("div");
+    divBlock.className = "tiles";
+    div.appendChild(divBlock);
+    divBlock.addEventListener('mouseover',(event)=>{
+        event.target.style.backgroundColor = "blueviolet";
+    });
+}
+
+resizeBtn = document.querySelector("#btn-resize");
+resizeBtn.addEventListener('click',()=>{
+    let num = prompt('Enter your desired grid size here ( size < 100 ) :');
+})
