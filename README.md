059e4a5f95b0edd6198e49c0dd79777c645ba268
Siddhant1423 <130544314+Siddhant1423@users.noreply.github.com>
2024-12-11 01:00:11 +0530

    Edit README file. Input validation for prompt and color randomizer added in JS file.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ The purpose of the project is to display DOM manipulation skills. HTML, CSS and 
 - flexbox is used for displaying grid.
 - `#container` div contains all the div `.tiles`.
 - Button on click event, will trigger a prompt to change grid size.
+- input validation for prompt. 
+> grid size should be less than 100
+- color randomizer for div.
--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.body.prepend(headingDiv);
 headingDiv.appendChild(heading)
 headingDiv.appendChild(btn)
 
+
 const div = document.querySelector("#container");
 
 for(let i = 0 ; i < 16*16 ; i++){
@@ -18,34 +19,46 @@ for(let i = 0 ; i < 16*16 ; i++){
     
     div.appendChild(divBlock);
     divBlock.addEventListener('mouseover',(event)=>{
-        event.target.style.backgroundColor = "blueviolet";
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        console.log(randomColor);
+        event.target.style.backgroundColor = "#"+randomColor ;
+
     });
 }
 
 const getNumber = () => {
-    let num = prompt('Enter your desired grid size here ( size < 100 ) :');
-    console.log(num)
-
+    while(true){
+        let num = prompt('Enter your desired grid size here ( size < 100 ) :');
+        if (num < 100){
+            return num;
+        }else {
+            alert("grid size should be less than 100")
+        }
+    }
+}
+const ChangeSize = () =>{
+    let number = getNumber();
+    console.log(number);
     let oldBlock = document.querySelectorAll(".tiles")
     oldBlock.forEach(block =>{
         block.remove();
     })
-    
-
-    for(let i = 0 ; i < num*num ;i++){
+    for(let i = 0 ; i < number*number ;i++){
         const newBlock = document.createElement("div");
-        let boxSize = (560 / num) - 2; 
+        let boxSize = (560 / number) - 2; 
         newBlock.style.height = newBlock.style.width = (boxSize) +'px';
         newBlock.className = "tiles";
         div.appendChild(newBlock);
         
         newBlock.addEventListener('mouseover',(event)=>{
-            event.target.style.backgroundColor = "blueviolet";
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            console.log(randomColor);
+            event.target.style.backgroundColor = "#"+randomColor;
         });
        } 
 }
 
 
 resizeBtn = document.querySelector("#btn-resize");
-resizeBtn.addEventListener('click',getNumber);
+resizeBtn.addEventListener('click',ChangeSize);
 
