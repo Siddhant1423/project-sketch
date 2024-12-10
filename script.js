a59858b9b8c1723819f11e98e26c73e2bafecaa1
Siddhant1423 <130544314+Siddhant1423@users.noreply.github.com>
2024-12-10 19:46:39 +0530

    modified README file. Made changes to script.js file.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ headingDiv.id = "heading";
 const heading = document.createElement("h1");
 heading.textContent = "Etch-a-Sketch";
 const btn = document.createElement("button");
-btn.textContent = "Change grid size"
+btn.textContent = "Change grid size";
 btn.id = "btn-resize";
+
 document.body.prepend(headingDiv);
 headingDiv.appendChild(heading)
 headingDiv.appendChild(btn)
@@ -14,13 +15,37 @@ const div = document.querySelector("#container");
 for(let i = 0 ; i < 16*16 ; i++){
     const divBlock = document.createElement("div");
     divBlock.className = "tiles";
+    
     div.appendChild(divBlock);
     divBlock.addEventListener('mouseover',(event)=>{
         event.target.style.backgroundColor = "blueviolet";
     });
 }
 
-resizeBtn = document.querySelector("#btn-resize");
-resizeBtn.addEventListener('click',()=>{
+const getNumber = () => {
     let num = prompt('Enter your desired grid size here ( size < 100 ) :');
-})
+    console.log(num)
+
+    let oldBlock = document.querySelectorAll(".tiles")
+    oldBlock.forEach(block =>{
+        block.remove();
+    })
+    
+
+    for(let i = 0 ; i < num*num ;i++){
+        const newBlock = document.createElement("div");
+        let boxSize = (560 / num) - 2; 
+        newBlock.style.height = newBlock.style.width = (boxSize) +'px';
+        newBlock.className = "tiles";
+        div.appendChild(newBlock);
+        
+        newBlock.addEventListener('mouseover',(event)=>{
+            event.target.style.backgroundColor = "blueviolet";
+        });
+       } 
+}
+
+
+resizeBtn = document.querySelector("#btn-resize");
+resizeBtn.addEventListener('click',getNumber);
+
